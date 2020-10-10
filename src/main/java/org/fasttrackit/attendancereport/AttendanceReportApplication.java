7ce53d537268e33df72816e12cbafea6bf189adf
Dragos Cont <dragoscont@live.com>
2020-10-10 14:15:47 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/attendancereport/AttendanceReportApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.attendancereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/attendancereport/AttendanceReportApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.attendancereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
